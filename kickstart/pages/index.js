3776e6111a9be4ab5e0e18d717efe6e4cf6b2934
Eyal <eyalpross101@gmail.com>
2022-04-29 22:07:05 +0300

    basic CSS

--- kickstart/pages/index.js
+++ kickstart/pages/index.js
@@ -1,16 +1,37 @@
 import React, { Component } from "react";
 import factory from "../ethereum/factory"; //return all campaigns relevant info...
 
+//ui-react package =>for styling
+import { Card } from "semantic-ui-react"; //javascript side
+//import 'semantic-ui-css/semantic.min.css'  //css side
+
 class CampaignIndex extends Component {
-
-  static async getInitialProps(){
+  static async getInitialProps() {
     const campaigns = await factory.methods.getDeployedCampaigns().call();
-    return {campaigns} //return as props
-  }  
+    return { campaigns }; //return as props
+  }
 
+  renderCampaigns() {
+    const items = this.props.campaigns.map((address) => {
+      return {
+        header: address,
+        description: <a>view Campaigns</a>,
+        fluid: true,
+      };
+    });
+    return <Card.Group items={items} />;
+  }
 
   render() {
-    return <div>{this.props.campaigns[0]}</div>;
+    return (
+      <div>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+        />
+        {this.renderCampaigns()}
+      </div>
+    );
   }
 }
 
