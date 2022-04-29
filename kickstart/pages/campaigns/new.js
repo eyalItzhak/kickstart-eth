17d953b8604fe2078c9ccd4b0680db7522655996
Eyal <eyalpross101@gmail.com>
2022-04-30 00:13:11 +0300

    add campaign

--- kickstart/pages/campaigns/new.js
+++ kickstart/pages/campaigns/new.js
@@ -1,20 +1,38 @@
-
-
 import React, { Component } from "react";
 import Layout from "../../components/Layout";
 
-import { Form, Button, Input } from "semantic-ui-react";
+import { Form, Button, Input, Message } from "semantic-ui-react";
+
+import factory from "../../ethereum/factory";
+import web3 from "../../ethereum/web3";
 
 class CampaignNew extends Component {
   state = {
     minimumContribution: "",
+    errorMessage: "",
+  };
+
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      
+      await factory.methods
+        .createCampaign(this.state.minimumContribution)
+        .send({
+          from: accounts[0],
+        });
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
   };
 
   render() {
     return (
       <Layout>
         <h3> Create a campaign</h3>
-        <Form>
+        <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>  
           <Form.Field>
             <label>Minimum Contribution</label>
             <Input
@@ -26,6 +44,7 @@ class CampaignNew extends Component {
               }
             />
           </Form.Field>
+          <Message error header="Oops!" content={this.state.errorMessage} />
           <Button primary type="submit">
             Create!
           </Button>
