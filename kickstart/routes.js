f245ef250a99f4b168cbe39f103a694b15799c9c
Eyal <eyalpross101@gmail.com>
2022-05-01 02:29:59 +0300

    added
    ContributeForm ,view request, make new request

--- kickstart/routes.js
+++ kickstart/routes.js
@@ -2,6 +2,9 @@ const routes = require("next-routes")();
 
 routes
   .add("/campaigns/new", "/campaigns/new")
-  .add("/campaigns/:address", "/campaigns/show"); //if user url look like "/campaigns/0xSomeAddress" we show the "show" page...
+  .add("/campaigns/:address", "/campaigns/show") //if user url look like "/campaigns/0xSomeAddress" we show the "show" page...
+  .add("/campaigns/:address/requests","/campaigns/requests/index")
+  .add("/campaigns/:address/requests/new","/campaigns/requests/new");
+
 
 module.exports = routes;
